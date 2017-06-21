4f599268b309650b206c26f818467df9002e28d3
engsupport <ydhole@appnexus.com>
2017-06-21 15:01:38 +0530

    Update NotifyEndofDowntime.java

--- src/main/java/org/appnexus/engsupportAPI/NotifyEndofDowntime.java
+++ src/main/java/org/appnexus/engsupportAPI/NotifyEndofDowntime.java
@@ -1,14 +1,22 @@
 package org.appnexus.engsupportAPI;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import org.quartz.TriggerKey;
+import org.quartz.impl.triggers.SimpleTriggerImpl;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class NotifyEndofDowntime implements Job {
-
-	RESTOperationImplementer restImpl;
-
+public class NotifyEndofDowntime implements Job{
+	
+	
+	
 	@Override
 	public void execute(JobExecutionContext jobContext)  {
 	
@@ -16,40 +24,37 @@ public class NotifyEndofDowntime implements Job {
 		String id = (String) jobContext.getJobDetail().getJobDataMap().get("id");
 		String until = (String) jobContext.getJobDetail().getJobDataMap().get("until");
 		
-		System.out.println("In Job execution >> "+until);
-				
-		// JSONText  response = new JSONText(restClient.callRestService(env.getProperty("niteowl.API.baseURL") + "/alerts/filter"));
-	     //@SuppressWarnings("unused")
-		//JSONText  response = ( (RESTOperationImplementer) jobContext.getJobDetail().getJobDataMap().get("callerInstance")) .getDownTimedAlerts();
-		/*String downtimed_alerts = response.getContent(); 
+	       JSONText  response = ( (RESTOperationImplementer) jobContext.getJobDetail().getJobDataMap().get("callerInstance")) .getAlertsFilter();
+		
+	       String downtimed_alerts = response.getContent(); 
    	  
 		 try {
 		 
 		 JSONParser parser = new JSONParser();
-         Object obj = parser.parse(downtimed_alerts);
+                 Object obj = parser.parse(downtimed_alerts);
 		
-         JSONObject jsonObject = (JSONObject) obj;
+                 JSONObject jsonObject = (JSONObject) obj;
 		 JSONArray response_array = (JSONArray) jsonObject.get("response");
 	   
 	 
-	       for (int i = 0; i < response_array.size(); i++) {
-	    	   
+	        for (int i = 0; i < response_array.size(); i++) {
 	    	   
 	    	   JSONObject row = (JSONObject) response_array.get(i);
 				 
 	    	   String existing_id = String.valueOf(row.get("id"));
 	    	   String existing_until = (String) (row.get("until"));
+	    	   
 			   if(existing_id.equals(id))
 					   {
 				   
 				    
+				   System.out.println(existing_until+" "+until + " "+existing_until.equals(until));
 				   if(existing_until.equals(until))
 				   {
 				   
 					  // Call Python url , pass user_name and Job id
 					   
-					   System.out.println(" From here I will do CURL");
-			
+				
 				   }
 				   else
 				   {
@@ -57,15 +62,13 @@ public class NotifyEndofDowntime implements Job {
 					  
 					   SimpleTriggerImpl simpleTrigger = new SimpleTriggerImpl();
 					   
-					   
 					   SimpleDateFormat  formatter = new SimpleDateFormat("yyyy-M-dd HH:mm:ss");
-					   formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 					   Date d1 = formatter.parse(existing_until);
 					   long timeMilli = d1.getTime();
 					   
 					   simpleTrigger.setStartTime(new Date(timeMilli - 600000));
 					   
-			           RESTOperationImplementer.scheduler.rescheduleJob(TriggerKey.triggerKey(id),simpleTrigger);
+			                   RESTOperationImplementer.scheduler.rescheduleJob(TriggerKey.triggerKey(id),simpleTrigger);
 					   
 					   return;
 					   
@@ -74,15 +77,16 @@ public class NotifyEndofDowntime implements Job {
 	       }else{
 	    	   
 	    	   //do nothing
-	    	   return;
+	    	   
 	       } 
+			   
 	       }
 		
 		 } catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		*/
+		
 		 
 		
 		
